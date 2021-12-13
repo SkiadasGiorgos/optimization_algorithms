369d6921cf2c9c437312412cf0db85442bff4aa8
George Skiadas <skiadas.giorgos04@gmail.com>
2021-12-13 18:42:02 +0000

    Lev_Marquardt_gamma_search - Final commit

---
+++ Minimization algorithms/Levenberg_Marquardt_golden_gamma.m
@@ -0,0 +1,46 @@
+clc 
+clear
+format long;
+starting_points = [[0,0];[-1,-1];[1,1]];
+epsilon = 0.001;
+ex_case = 2;
+k = 1;
+
+if ex_case == 1
+    x0(k) = starting_points(1,1);
+    y0(k) = starting_points(1,2);
+elseif ex_case == 2
+    x0(k) = starting_points(2,1);
+    y0(k) = starting_points(2,2);
+elseif ex_case == 3
+    x0(k) = starting_points(3,1);
+    y0(k) = starting_points(3,2);
+end
+
+
+x_solve(k) = func(x0(k),y0(k));
+
+while norm(f_grad(x0(k),y0(k))) >= epsilon
+    f_hess = f_hessian(x0(k),y0(k));
+    meu = 0;
+    H = f_hess + meu*eye(size(f_hess));
+    while H(1,1)<0 || H(1,1)*H(2,2)-H(1,2)*H(2,1)<0
+        meu = meu+1;
+        H = f_hess + meu*eye(size(f_hess));        
+    end
+    fgrad =f_grad(x0(k),y0(k));
+    d = -fgrad/H;
+    gamma = golden_search_gamma(x0(k),y0(k),d);
+    x0(k+1) = x0(k) + gamma*d(1);
+    y0(k+1) = y0(k) + gamma*d(2);
+    k = k+1;
+    x_solve(k) = func(x0(k),y0(k));
+end
+
+answer = x_solve(end);
+
+figure(1)
+plot(1:k,x_solve,'-x')
+title('Levenberg Marquardt - Golden Gamma')
+xlabel('Number of repetitions')
+ylabel('f(x,y)')
